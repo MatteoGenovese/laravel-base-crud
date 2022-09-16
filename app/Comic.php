ffc9e8e6fed6033e90c4bba6dd72ef2448b38ad7
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-16 16:39:40 +0200

    init: fillable in Comic Model

--- app/Comic.php
+++ app/Comic.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     //
+    protected $fillable = [
+
+        'title',
+        'slug',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'sale_date',
+        'type',
+    ];
 }
