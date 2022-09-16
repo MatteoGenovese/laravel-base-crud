d4dce6893473e6522b82072ca6ce0e287073dd2a
MatteoGenovese <matteo.genovese@icloud.com>
2022-09-16 08:29:17 +0200

    init : DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ComicsTableSeeder::class);
+        $this->call([
+            ComicsTableSeeder::class
+        ]);
     }
 }
